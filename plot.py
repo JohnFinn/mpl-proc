d8ca88ef65ebc6fe9ec93a73d2074f4e7102634a
Jouni Sunnari <dz4tune@gmail.com>
2021-03-19 04:33:10 +0300

    better event handling

--- plot.py
+++ plot.py
@@ -12,25 +12,33 @@ fig, ax = plt.subplots()
 
 x_data = []
 lines = [
-    plt.plot([], [], label=name)[0] for name in sys.argv[1:]
+    ax.plot([], [], label=name)[0] for name in sys.argv[1:]
 ]
 
 y_data = [[] for _ in sys.argv[1:]]
 max_x = 1
+max_y, min_y = 0, 1
 crt_lim = 1
 
 def init():
     ax.set_xlim(0, 1)
-    ax.set_ylim(0, 20)
+    ax.set_ylim(10, 20)
     return lines
 
 def update(nums):
     global crt_lim
     global max_x
+    global max_y
+    global min_y
     max_x += 1
+    crt_min_y, crt_max_y = min(nums), max(nums)
     if max_x >= crt_lim:
-        crt_lim *= 8
+        crt_lim *= 2
         ax.set_xlim(0, crt_lim)
+    if crt_max_y > max_y or crt_min_y < min_y:
+        min_y = min(min_y, crt_min_y)
+        max_y = max(crt_max_y, max_y)
+        ax.set_ylim(top = max_y * 1.1, bottom = min_y * 1.1)
 
     x_data.append(max_x)
     # ax.set_ylim(min_y * 1.1, max_y * 1.1)
@@ -50,11 +58,11 @@ if __name__ == '__main__':
     poller.register(0, select.POLLIN)
 
     while True:
-        res = poller.poll(0)
-        if res:
-            line = input()
-            nums = [float(i) for i in line.split()]
-            update(nums)
+        if poller.poll(0):
+            while poller.poll(0):
+                line = sys.stdin.readline()
+                nums = [float(i) for i in line.split()]
+                update(nums)
             fig.canvas.draw()
             fig.canvas.flush_events()
         else:
